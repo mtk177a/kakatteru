f12b709b821d64c6352d3b8dbacd3889e28d7a6a
mtk177a <mtk177a@gmail.com>
2024-05-05 01:11:55 +0900

    ログインとログアウトのルーティングを更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   root "users#index"
   resources :users
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
