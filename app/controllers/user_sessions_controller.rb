5bb3d9170e093ae64656777250ae28cd72c0bd07
mtk177a <mtk177a@gmail.com>
2024-05-05 13:17:41 +0900

    ビューのデザイン調整

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,15 +7,15 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(:users, notice: 'Login successful')
+      redirect_to root_path, success: 'Login successful'
     else
-      flash.now[:alert] = 'Login failed'
-      render action: 'new'
+      flash.now[:error] = 'Login failed'
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     logout
-    redirect_to(:users, notice: 'Logged out!')
+    redirect_to root_path, status: :see_other, error: 'Logged out!'
   end
 end
