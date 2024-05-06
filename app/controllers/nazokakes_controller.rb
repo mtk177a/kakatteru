89a9fcd927c6b7f6d6d9b94b8872cd31d2c63a82
mtk177a <mtk177a@gmail.com>
2024-05-07 02:26:18 +0900

    Twitterシェア機能の実装

--- app/controllers/nazokakes_controller.rb
+++ app/controllers/nazokakes_controller.rb
@@ -3,6 +3,8 @@ class NazokakesController < ApplicationController
     @question = Question.random
     @answer = Answer.random
     @heart = Heart.random
+
+    tweet_content = "**ランダムなぞかけ**\n\n【#{@question.content}】とかけて\n【#{@answer.content}】と解く\nその心は、どちらも【#{@heart.content}】\n#かかってる #なぞかけ\n\nkakatteru.onrender.com"
+    @encoded_text = URI.encode_www_form_component(tweet_content)
   end
 end
-
