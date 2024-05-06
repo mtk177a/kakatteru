a009af5762a29e4cb6c3c424250536b5ad0d1400
mtk177a <mtk177a@gmail.com>
2024-05-06 23:16:28 +0900

    各投稿モデルにランダム選択ロジックの追加

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :user
+
+  def self.random
+    order(Arel.sql('RANDOM()')).first
+  end
 end
--- app/models/heart.rb
+++ app/models/heart.rb
@@ -1,3 +1,7 @@
 class Heart < ApplicationRecord
   belongs_to :user
+
+  def self.random
+    order(Arel.sql('RANDOM()')).first
+  end
 end
